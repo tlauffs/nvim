40561ebf315c824b64a30e91abb40c8058123439
tlauffs <timlauffs@proton.me>
2025-12-04 17:23:46 +0100

    balls

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	main = "nvim-treesitter.configs", -- Sets main module to use for opts
 	opts = {
 		ensure_installed = {
 			"bash",
@@ -14,6 +15,10 @@ return { -- Highlight, edit, and navigate code
 			"query",
 			"vim",
 			"vimdoc",
+			"python",
+			"javascript",
+			"typescript",
+			"php",
 		},
 		-- Autoinstall languages that are not installed
 		auto_install = true,
